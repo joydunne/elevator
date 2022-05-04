39d484620036e745c5b58649b9de4fccb00bf682
joydunne <joydunne33@gmail.com>
2022-05-04 15:12:19 -0500

    sleep time

--- button.py
+++ button.py
@@ -5,7 +5,7 @@ import time
 buttonOne= Button(2)
 buttonTwo= Button(3)
 while True:
-    time.sleep (10000)
+    time.sleep (1)
     if buttonOne.is_pressed:
         print("Button One is pressed")
     if buttonTwo.is_pressed:
