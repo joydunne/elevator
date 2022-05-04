1e9a85e6b1a77b4f90d53bcd625c09e24aa15399
joydunne <joydunne33@gmail.com>
2022-05-04 15:00:27 -0500

    fix

--- button.py
+++ button.py
@@ -1,5 +1,11 @@
-from gpiozero import Button
-button = Button(2)
 
-button.wait_for_press()
-print('You pushed me')
+from gpiozero import Button
+import time
+
+buttonOne= Button(2)
+buttonTwo= Button(3)
+while True:
+    if buttonOne.is_pressed:
+        print("Button One is pressed")
+    if buttonTwo.is_pressed:
+        print("Button Two is pressed")
