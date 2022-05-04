f8cb5e854816914efa48bf520dc65f3b19014427
joydunne <joydunne33@gmail.com>
2022-05-04 15:18:09 -0500

    update

--- button.py
+++ button.py
@@ -1,12 +1,22 @@
 
 from gpiozero import Button
 import time
-
+from motorU import *
+elevatorMotor = motorU()
+pos = "down"
 buttonOne= Button(2)
 buttonTwo= Button(3)
 while True:
     time.sleep (0.5)
     if buttonOne.is_pressed:
         print("Button One is pressed")
+        if pos == "down":
+            pos = "up"
+            for i in range (4):
+        	       windowMotor.rotate()
     if buttonTwo.is_pressed:
         print("Button Two is pressed")
+        if pos == "up":
+            pos = "down"
+            for i in range (4):
+        	       windowMotor.rotate(direction = "counterClockwise")
